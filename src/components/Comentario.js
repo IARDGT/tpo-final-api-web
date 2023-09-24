b8df27c9bbf5ab5cc0a4dd6a799f6f8286dfbf43
reborafs <reborafs@gmail.com>
2023-09-24 19:54:07 -0300

    creo pagina ver-clase

---
+++ src/components/Comentario.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export const Comentario = () => {
+
+    return (
+        <div class="card mx-5 my-2 px-3">
+            <div class="card-body">
+                <h5 class="card-title">Comentario ejemplo</h5>
+                <p class="card-text">Este es un comentario a modo de ejemplo. El alumno debe llenar un comentario sobre la clase para que pueda figurar en esta pagina.</p>
+                <div class="row">
+                    <p class="card-text col-6"><small class="text-muted">Escrito por Franco Rébora</small></p>
+                    <p class="card-text col-6 text-end"><small class="text-muted ">Última actualización hace 3 minutos</small></p>
+                </div>
+
+            </div>
+        </div>
+    )
+
+}
+
+export default Comentario;
