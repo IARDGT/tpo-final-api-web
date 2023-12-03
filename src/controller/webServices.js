af44acf581b3005e05df1f351a583131d5b41116
reborafs <reborafs@gmail.com>
2023-12-02 22:52:11 -0300

    Fin de mi-perfil

--- src/controller/webServices.js
+++ src/controller/webServices.js
@@ -6,7 +6,7 @@ const urlWebServices = {
     register:urlApi +"api/users/registration",
     getUserById:urlApi +"api/users/userById",
     uploadProfileImage:urlApi +"api/users/image-upload",
-    updateProfileImage:urlApi +"api/users/update",
+    updateProfile:urlApi +"api/users/update",
     catalogo:urlApi+"api/clase/catalogo",
     verClase:urlApi+"api/clase/ver-clase/",
     updateComentario:urlApi+"api/clase/actualizar-comment/",
